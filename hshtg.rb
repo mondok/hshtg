af2d8332c2df2612e666713eb82819860d336509
Matt Mondok <mattm@edentech.net>
2015-03-14 19:44:12 -0400

    Introduce guided setup for keys

--- hshtg.rb
+++ hshtg.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require_relative 'lib/hshtg'
+require_relative 'lib/hshtg/user_setup'
 
 # Check for .env file
 Hshtg::Utils.load_env_vars
@@ -9,6 +10,7 @@ if __FILE__== $PROGRAM_NAME
   if Hshtg::Configuration.is_valid?
     Hshtg::ServerBootstrapper.start
   else
-    Hshtg::Utils.no_keys_error_message
+    user_setup = Hshtg::UserSetup.new(Hshtg::Configuration.env_variable_names)
+    user_setup.try_help
   end
 end
