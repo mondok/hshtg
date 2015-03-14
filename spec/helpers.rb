54884fb90e1b0c381db722ec5aa8759952078a4e
Matt Mondok <mattm@edentech.net>
2015-03-14 11:43:39 -0400

    Hashtag extraction code

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,19 +1,10 @@
 module Helpers
-  def tweet_text_1
-    # tweet: 3
-    # tweets: 1
-    # text: 2
-    # one: 1
-    # two: 1
-    # three: 1
-    # total hashtags: 9
-    'Here is some #tweet #text that contains #tweets and it is a #tweet text #tweet #one #two #three # nothing #text'
-  end
-
-  def tweet_text_2
-    # test: 4
-    # tweet: 1
-    # total: 5
-    '#test one #test two #test three #test four #tweet'
+  def hashtag_list
+    [
+      Hshtg::Models::Hashtag.new('one'),
+      Hshtg::Models::Hashtag.new('one'),
+      Hshtg::Models::Hashtag.new('two'),
+      Hshtg::Models::Hashtag.new('three')
+    ]
   end
 end
--- spec/in_memory_store_spec.rb
+++ spec/in_memory_store_spec.rb
@@ -5,7 +5,46 @@ RSpec.describe Hshtg::Storage, '#memory_store', focus: true do
       expect(store).to_not be_nil
     end
 
+    it 'can add Hashtags' do
+      store = Hshtg::Storage::InMemoryStore.new
+      store.add_tags(hashtag_list)
+      expect(hashtag_list.count).to be > 0
+      expect(store.all.count).to eq(hashtag_list.count)
+    end
 
+    it 'can get fresh hashtags' do
+      store = Hshtg::Storage::InMemoryStore.new
+      list = hashtag_list
+      list[0].timestamp = Time.now - 100
+      store.add_tags(list)
+      expect(store.fresh_tags.count).to eq(list.count-1)
+    end
+
+    it 'can evict stale hashtags' do
+      store = Hshtg::Storage::InMemoryStore.new
+      list = hashtag_list
+      list[0].timestamp = Time.now - 100
+      store.add_tags(list)
+      store.evict_old_tags
+      expect(store.fresh_tags.count).to eq(list.count-1)
+    end
+
+    it 'can get fresh hashtags with custom time' do
+      store = Hshtg::Storage::InMemoryStore.new
+      list = hashtag_list
+      list[0].timestamp = Time.now - 100
+      store.add_tags(list)
+      expect(store.fresh_tags(120).count).to eq(list.count)
+    end
+
+    it 'can evict stale hashtags with custom time' do
+      store = Hshtg::Storage::InMemoryStore.new
+      list = hashtag_list
+      list[0].timestamp = Time.now - 100
+      store.add_tags(list)
+      store.evict_old_tags(10)
+      expect(store.fresh_tags.count).to eq(list.count-1)
+    end
 
   end
 
