5c68d2cad933f0d571d6dcd2a9c939e209fe8fbb
Matt Mondok <mattm@edentech.net>
2015-03-14 10:33:54 -0400

    Load .env file if exists

--- hshtg.rb
+++ hshtg.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 
 require_relative 'lib/hshtg'
+
+# Check for .env file
+if File.exist?('.env')
+  File.open('.env', 'r').each_line do |line|
+    a = line.chomp("\n").split('=', 2)
+    a[1].gsub!(/^"|"$/, '') if ['\'', '"'].include?(a[1][0])
+    eval "ENV['#{a[0]}']='#{a[1] || ''}'"
+  end
+end
